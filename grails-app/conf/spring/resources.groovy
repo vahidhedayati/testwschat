7f9400256008f650449b78da345f41623712f1b6
Vahid <vahid.hedayati@gmail.com>
2015-01-02 20:16:11 +0000

    override wsClientProcessService tested/working under 2.4.4

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -4,13 +4,6 @@ import anythingbut.grails.plugin.wschat.MyOverrideService
 
 // Place your Spring DSL code here
 beans = {
-	/*
-	wsClientProcessService(MyChatClientService){
-		grailsApplication = ref('grailsApplication')
-		chatClientListenerService = ref('chatClientListenerService')
-		wsChatUserService = ref('wsChatUserService')
-	}
-	*/
 	wsClientProcessService(MyOverrideService){
 		grailsApplication = ref('grailsApplication')
 		chatClientListenerService = ref('chatClientListenerService')
